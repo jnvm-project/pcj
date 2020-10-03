4b8ef1507b39586519657073c42a831591450f47
Anatole Lefort <anatole.lefort@telecom-sudparis.eu>
2020-10-03 20:39:40 +0000

    include native library inside the jar

--- tools/mvn/install.sh
+++ tools/mvn/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-jar cvf pcj.jar -C target/classes lib/
+jar cvf pcj.jar -C target/classes lib/ -C target/cppbuild libPersistent.so
 
 mvn install:install-file -Dfile=pcj.jar -DgroupId=com.intel -DartifactId=pcj -Dversion=1.0 -Dpackaging=jar
